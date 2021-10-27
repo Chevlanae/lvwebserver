01ec354ceef7ffbe5f7a74a804511ba51f2f3930
Chevlanae <leahvance6312@gmail.com>
2021-10-27 20:13:03 +0000

    worked out more features, added favicon and browserify

--- app.js
+++ app.js
@@ -1,193 +1,210 @@
-const express = require('express');
-const session = require('express-session');
-const { body, matchedData } = require('express-validator');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const https = require('https');
-const argon2 = require('argon2');
-const { MongoClient } = require('mongodb');
-const MongoStore = require('connect-mongo');
-const assert = require('assert');
-const fs = require('fs');
-const Environment = require('./env.json');
-
-
+const express = require("express");
+const session = require("express-session");
+const { body, matchedData } = require("express-validator");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
+const https = require("https");
+const argon2 = require("argon2");
+const { MongoClient } = require("mongodb");
+const MongoStore = require("connect-mongo");
+const assert = require("assert");
+const fs = require("fs");
+const Environment = require("./env.json");
 
 //process args
 var port;
 process.argv.forEach((val, index) => {
-  if(val == '--port'){
-    port = new Number(argv[index + 1]);
-  }
+	if (val == "--port") {
+		try {
+			port = new Number(argv[index + 1]);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 });
-if(port == undefined){
-  port = 443;
+if (port == undefined) {
+	port = 443;
 }
 
 //db init
 const dbClient = new MongoClient(Environment.dbURL);
-dbClient.connect((err, result) => {
-  assert.equal(null, err);
-  console.log('Successfully connected to DB');
+dbClient.connect((err) => {
+	assert.equal(null, err);
+	console.log("Successfully connected to DB");
 
-  const db = result.db('fileserver');
-  const Users = db.collection('users', {strict: true});
-  Users.createIndex('username').then(() => result.close());
+	const db = dbClient.db("fileserver");
+	const Users = db.collection("users", { strict: true });
+	Users.createIndex("username").then(() => dbClient.close());
 });
 
-const hashPassword = async (password) => argon2.hash(password, {
-  type: argon2.argon2id,
-  memoryCost: 2 ** 16,
-});
+const hashPassword = async (password) =>
+	argon2.hash(password, {
+		type: argon2.argon2id,
+		memoryCost: 2 ** 16,
+	});
+
+const secretArr = () => {
+	////Shuffle array of session secrets
+	let array = Environment.sessionSecrets;
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+	return array;
+};
+
+const resJSON = (message, error) => {};
 
 //server init
 var app = express();
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(session({
-  cookie: {secure: true, httpOnly: true, samesite: true, maxAge: 600000},
-  resave: false,
-  saveUninitialized: false,
-  name: 'server.sid',
-  secret: () => {
-    ////Shuffle array of session secrets
-    let array = Environment.secrets
-    let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array;
-  },
-  store: MongoStore.create({
-    mongoUrl: Environment.dbURL,
-    crypto: {
-      secret: Environment.secrets[0]
-    }
-  })
-}));
+app.use("/static", express.static("./static"));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(favicon(__dirname + "/static/images/favicon.ico"));
+app.use(
+	session({
+		cookie: { secure: true, httpOnly: true, samesite: true, maxAge: 600000 },
+		resave: false,
+		saveUninitialized: true,
+		name: "server.sid",
+		secret: secretArr(),
+		store: MongoStore.create({
+			mongoUrl: Environment.dbURL,
+			crypto: {
+				secret: Environment.storeSecret,
+			},
+		}),
+	}),
+);
 
 //configure cors
 var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (serverVar.originAllowList.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } 
-  else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+	var corsOptions;
+	if (serverVar.originAllowList.indexOf(req.header("Origin")) !== -1) {
+		corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+	} else {
+		corsOptions = { origin: false }; // disable CORS for this request
+	}
+	callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 //authorization check
-function checkSignIn(req, res){
-  if(req.session.user){
-     return;     //If session exists, proceed to page
-  } else {
-     res.redirect('/');
-  }
+function checkSignIn(req, res, next) {
+	if (req.session.user) {
+		next(); //If session exists, proceed to page
+	} else {
+		res.redirect("/login");
+	}
 }
 
-
-app.get('/', checkSignIn, cors(corsOptionsDelegate), function(req, res){
-  res.sendFile('/html/index.html', {root: __dirname});
+app.get("/", checkSignIn, cors(corsOptionsDelegate), function (req, res) {
+	res.sendFile("/html/index.html", { root: __dirname });
 });
 
-app.get('/signup' , function(req, res){
-  res.sendFile('/html/signup/index.html', {root: __dirname});
+app.get("/signup", function (req, res) {
+	res.sendFile("/html/signup/index.html", { root: __dirname });
 });
 
-app.post('/signup',
-  body('email').isString().isEmail().normalizeEmail(),
-  body('username').isString().escape(),
-  body('password').isString(),
-  function(req, res){
+app.post(
+	"/signup",
+	body("email").isString().isEmail().normalizeEmail(),
+	body("username").isString().escape(),
+	body("password").isString(),
+	function (req, res) {
+		var userData = matchedData(req, { locations: ["body"] });
 
-    var userData = matchedData(req, { locations: ['body'] });
-    console.log(userData);
-    dbClient.connect((err, result) => {
-      if(err){
-        console.error(error);
-      }
+		if (
+			!("email" in userData && "username" in userData && "password" in userData)
+		) {
+			res.status(400).send();
+		}
 
-      var users = result.db('fileserver').collection('users');
-      var userExists = false;
-      users.find({'username': userData.username}).forEach(() => {userExists = true}).then(() => {
+		dbClient.connect((err) => {
+			assert.equal(null, err);
 
-        if(!userExists && typeof userData.password === typeof ''){
-          hashPassword(userData.password).then((hashedPassword) => {
-            userData.password = hashedPassword;
-            users.insertOne(userData);
-  
-            req.session.user = unescape(userData.username);
-            res.render('./html/signup/confirm_email.html', {username: req.body.username});
-          });
-        }
-        else{
-          res.status(400).send('User already exists');
-        }
+			var users = dbClient.db("fileserver").collection("users");
+			var userExists = false;
+			users
+				.find({ username: userData.username })
+				.forEach(() => {
+					userExists = true;
+				})
+				.then(() => {
+					if (!userExists) {
+						hashPassword(userData.password).then((hashedPassword) => {
+							userData.password = hashedPassword;
 
-        result.close();
-      });
-    });
-  }
+							users.insertOne(userData);
+
+							req.session.user = unescape(userData.username);
+							res.sendFile("./html/signup/confirm_email.html", {
+								root: __dirname,
+							});
+						});
+					} else {
+						res.status(400).send("User already exists");
+					}
+				});
+		});
+	},
 );
 
-app.get('/login', function(req, res){
-  res.sendFile('/html/login/index.html', {root: __dirname});
+app.get("/login", function (req, res) {
+	res.sendFile("/html/login/index.html", { root: __dirname });
 });
 
-app.post('/login', 
-  body('username').isString().escape(),
-  body('password').isString(),
-  function(req, res){
+app.post(
+	"/login",
+	body("username").isString().escape(),
+	body("password").isString(),
+	function (req, res) {
+		var userData = matchedData(req, { locations: ["body"] });
 
-    var userData = matchedData(req, { locations: ['body'] });
+		dbClient.connect((err) => {
+			assert.equal(null, err);
 
-    dbClient.connect((err, result) => {
-      if(err){
-        console.log(err);
-      }
-      
-      var users = result.db('fileserver').collection('users');
-      users.findOne({'username': userData.username}).then((user) => {
+			var users = dbClient.db("fileserver").collection("users");
+			users.findOne({ username: userData.username }).then((user) => {
+				if (user == null) {
+					res.status(400).send("Invalid Username");
+					return;
+				}
 
-        if(user == null){
-          res.status(400).send('Invalid Username');
-          return;
-        }
-  
-        hashPassword(userData.password).then((hashedPassword) => {
-          if(hashedPassword === user.password){
-            req.session.user = userData.username;
-            res.render('./html/index.html');
-          }
-          else {
-            res.status(400).send('Invalid Password');
-          }
-          result.close();
-        });
-      });
-    });
-  }
+				hashPassword(userData.password).then((hashedPassword) => {
+					console.log(hashedPassword, user.password);
+					if (hashedPassword === user.password) {
+						req.session.user = userData.username;
+						res.redirect("/");
+					} else {
+						res.status(400).send("Invalid Password");
+					}
+					dbClient.close();
+				});
+			});
+		});
+	},
 );
 
 //https config
 var options = {
-  key: fs.readFileSync(Environment.SSL.key),
-  cert: fs.readFileSync(Environment.SSL.cert)
+	key: fs.readFileSync(Environment.SSL.key),
+	cert: fs.readFileSync(Environment.SSL.cert),
 };
 
 var server = https.createServer(options, app);
 
 server.listen(port, () => {
-  console.log('Server started on port: ' + port.toString());
+	console.log("Server started on port: " + port.toString());
 });
---
+++ html/signup/scripts/index.js
@@ -0,0 +1,97 @@
+const needle = require("needle");
+const swal = require("sweetalert2");
+
+const origin = window.origin;
+
+var emailConfirmed = false;
+var passwordConfirmed = false;
+
+function signup() {
+	var email = document.getElementById("email").value,
+		username = document.getElementById("username").value,
+		password = document.getElementById("password").value;
+
+	if (emailConfirmed && passwordConfirmed) {
+		var url = origin + "/signup",
+			options = { json: true },
+			body = {
+				email: email,
+				username: username,
+				password: password,
+			};
+
+		needle.post(url, body, options, (err, res) => {
+			if (err) {
+				swal.fire("Error", err.toString(), "error");
+			}
+
+			if (res) {
+				if (res.headers["content-type"] == "application/json") {
+					if ("errors" in res.body) {
+						var errorString = "<p>";
+						for (var error in res.body.errors) {
+							errorString = errorString + error.msg + "<br>";
+						}
+						errorString = errorString + "</p>";
+
+						swal.fire({
+							title: "Error",
+							html: errorString,
+							icon: "error",
+						});
+					}
+				} else if (
+					res.statusCode == 200 &&
+					res.headers["content-type"] == "text/html"
+				) {
+					document.write(res.body);
+				}
+			}
+		});
+	}
+}
+
+function confirm(original, confirmed, msgbox) {
+	var originalElement = document.getElementById(original),
+		confirmedElement = document.getElementById(confirmed),
+		msgboxElement = document.getElementById(msgbox);
+
+	if (originalElement.value == "" || confirmedElement.value == "") {
+		originalElement.setAttribute("style", "");
+		confirmedElement.setAttribute("style", "");
+		return;
+	} else if (originalElement.value != confirmedElement.value) {
+		msgbox.innerHTML = `<span style="color: red;"><strong>Fields do not match!</strong><span>`;
+		originalElement.setAttribute(
+			"style",
+			"border: 1px solid rgba(219, 33, 20, 1);",
+		);
+		confirmedElement.setAttribute(
+			"style",
+			"border: 1px solid rgba(219, 33, 20, 1);",
+		);
+
+		if (original == "email") {
+			emailConfirmed = false;
+		} else if (original == "password") {
+			passwordConfirmed = false;
+		}
+	} else if (originalElement.value == confirmedElement.value) {
+		msgbox.innerHTML = `<span style="color: green;"><strong>Fields match.</strong><span>`;
+		originalElement.setAttribute("style", "");
+		confirmedElement.setAttribute("style", "");
+
+		if (original == "email") {
+			emailConfirmed = true;
+		} else if (original == "password") {
+			passwordConfirmed = true;
+		}
+	}
+}
+
+//event listeners
+
+module.exports = {
+	signup: signup,
+	confirm: confirm,
+};
